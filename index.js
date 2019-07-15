d7085318fbfb23c2268889d67566cc298aa76717
Dani Hodovic <dani.hodovic@gmail.com>
2019-07-15 20:13:03 +0800

    Add tests

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 #!/usr/bin/env node
-const mjml = require('mjml')
-const express = require('express')
-const argv = require('./lib/parse_args.js').argv
-const { middleware, logger } = require('./lib/logging.js')
+const { logger } = require('./lib/logging.js')
 const graceful = require('node-graceful')
 
-const app = express()
-app.use(middleware)
-app.post('/', (req, res) => {
-  try {
-    const result = mjml(req.body, {
-      keepComments: argv.keepComments,
-      beautify: argv.beautify,
-      minify: argv.minify,
-      validationLevel: argv.validationLevel
-    })
-    res.send(result)
-  } catch (err) {
-    logger.error('Failed to compile mjml', err)
-    res.statusCode = 400
-    res.send('Encountered an error while compiling mjml. Check your server logs')
-  }
-})
+const { create } = require('./lib/server.js')
+const argv = require('./lib/parse_args.js').argv
 
-const server = app.listen(argv.port, argv.host, () => {
+const server = create(argv).listen(argv.port, argv.host, () => {
   logger.info(`Starting server on port ${argv.port}`)
 })
 
